ee220bf0d10cf6eb22fecca413f816fc453ce8f3
tz <1204216974@qq.com>
2020-09-13 13:50:03 +0800

    界面UI

--- app/src/main/java/com/example/studentmagicbox/MainActivity.java
+++ app/src/main/java/com/example/studentmagicbox/MainActivity.java
@@ -3,14 +3,25 @@ import com.example.studentmagicbox.Beans.*;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chaquo.python.PyObject;
@@ -25,6 +36,111 @@ public class MainActivity extends AppCompatActivity {
     EditText userlogin;
     EditText userpassword;
     Button check;
+
+    private TextView mBtnLogin;
+    private View progress;
+    private View mInputLayout;
+    private float mWidth,mHeight;
+    private LinearLayout mName,mPsw;
+
+    private void InitView()
+    {
+        mBtnLogin=findViewById(R.id.main_btn_login);
+        progress=findViewById(R.id.layout_progress);
+        mInputLayout=findViewById(R.id.input_layout);
+        mName=findViewById(R.id.input_layout_name);
+        mPsw=findViewById(R.id.input_layout_psw);
+        userlogin=findViewById(R.id.loginname);
+        userpassword=findViewById(R.id.password);
+    }
+
+
+
+    /**
+     * 输入框的动画效果
+     *
+     * @param view
+     *            控件
+     * @param w
+     *            宽
+     * @param h
+     *            高
+     */
+    private void inputAnimator(final View view, float w, float h) {
+
+        AnimatorSet set = new AnimatorSet();
+
+        ValueAnimator animator = ValueAnimator.ofFloat(0, w);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (Float) animation.getAnimatedValue();
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) view
+                        .getLayoutParams();
+                params.leftMargin = (int) value;
+                params.rightMargin = (int) value;
+                view.setLayoutParams(params);
+            }
+        });
+
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mInputLayout,
+                "scaleX", 1f, 0.5f);
+        set.setDuration(1000);
+        set.setInterpolator(new AccelerateDecelerateInterpolator());
+        set.playTogether(animator, animator2);
+        set.start();
+        set.addListener(new Animator.AnimatorListener() {
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                /**
+                 * 动画结束后，先显示加载的动画，然后再隐藏输入框
+                 */
+                progress.setVisibility(View.VISIBLE);
+                progressAnimator(progress);
+                mInputLayout.setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+        });
+
+    }
+
+
+    /**
+     * 出现进度动画
+     *
+     * @param view
+     */
+    private void progressAnimator(final View view) {
+        PropertyValuesHolder animator = PropertyValuesHolder.ofFloat("scaleX",
+                0.5f, 1f);
+        PropertyValuesHolder animator2 = PropertyValuesHolder.ofFloat("scaleY",
+                0.5f, 1f);
+        ObjectAnimator animator3 = ObjectAnimator.ofPropertyValuesHolder(view,
+                animator, animator2);
+        animator3.setDuration(1000);
+        animator3.setInterpolator(new JellyInterpolator());
+        animator3.start();
+
+    }
+
+
     private int  SuccessCount=0;
 
     private void SkipTo()
@@ -51,23 +167,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initPython();
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        userlogin=(EditText)findViewById(R.id.userlogin);
-        userpassword=(EditText)findViewById(R.id.userpassword);
-        check=findViewById(R.id.check);
+        InitView();
         new LoadingThread().start();
         new EmailThread().start();
         new ClassRoomThread().start();
         new ScheduleThread().start();
-        check.setOnClickListener(evt->
+        mBtnLogin.setOnClickListener(evt->
         {
+            // 计算出控件的高与宽
+            mWidth = mBtnLogin.getMeasuredWidth();
+            mHeight = mBtnLogin.getMeasuredHeight();
+            // 隐藏输入框
+            mName.setVisibility(View.INVISIBLE);
+            mPsw.setVisibility(View.INVISIBLE);
+
+            inputAnimator(mInputLayout, mWidth, mHeight);
+
+
             loginname=userlogin.getText().toString();
             password=userpassword.getText().toString();
+
+
             patchHandler.sendEmptyMessageDelayed(1,0);
             classHandler.sendEmptyMessageDelayed(1,3000);
             scheduleHander.sendEmptyMessageDelayed(1,6000);
             emailHandler.sendEmptyMessageDelayed(1,9000);
         });
+    }
+
+
+    private void recovery()
+    {
+        progress.setVisibility(View.GONE);
+        mInputLayout.setVisibility(View.VISIBLE);
+        mName.setVisibility(View.VISIBLE);
+        mPsw.setVisibility(View.VISIBLE);
+
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) mInputLayout.getLayoutParams();
+        params.leftMargin = 0;
+        params.rightMargin = 0;
+        mInputLayout.setLayoutParams(params);
+
+
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mInputLayout, "scaleX", 0.5f,1f );
+        animator2.setDuration(500);
+        animator2.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator2.start();
     }
 
 
